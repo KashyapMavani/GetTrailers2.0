d0a0c9251687160e590ab14bdb2c71098c03a884
KashyapMavani <kmavani1807@gmail.com>
2024-01-09 23:20:27 +0530

    ...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -75,6 +75,7 @@ const Header = () => {
             <ContentWrapper>
                 <div className="logo" onClick={() => navigate("/")}>
                     <img src={logo} alt="" />
+                    <span className="logo-text">Chillzz</span>
                 </div>
                 <ul className="menuItems">
                     <li
